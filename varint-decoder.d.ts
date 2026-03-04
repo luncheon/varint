3153f1517e00129c7d1ccdd5e48d66776be1e61f
luncheon <takeuck+github@gmail.com>
2026-03-04 20:32:37 +0900

    fix: typing: allow `number` chunks

--- varint-decoder.d.ts
+++ varint-decoder.d.ts
@@ -1,1 +1,1 @@
-export function varintDecoder(chunkSize: number, zigzag?: boolean): (chunkSequence: Iterator<bigint> | Iterable<bigint>) => bigint;
+export function varintDecoder(chunkSize: number, zigzag?: boolean): (chunkSequence: Iterator<number | bigint> | Iterable<number | bigint>) => bigint;
